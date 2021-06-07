ee2a963e236d2de9907b416278882a81966518b9
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-07 11:21:36 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,104 @@
+//Array of objects
+const quiz = [
+    {
+        q:" ....... she got a bike? ",
+        options:["Has", "-", "Have", "Does"],
+        answer: 0
+    },
+    {
+        q:" ....... you from Moscow? ",
+        options:["Come", "Are", "Is", "Do"],
+        answer: 1
+    },
+    {
+        q:" ....... you like cats?",
+        options:["Have", "Are", "Do", "Does"],
+        answer: 2
+    },
+    {
+        q:" We tell him what to do, but he never listens to .......  .",
+        options:["our", "them", "we", "us" ],
+        answer: 3
+    },
+    {
+        q:" This is my sister.  ....... name is Anna.",
+        options:["Her", "His", "She", "Our"],
+        answer: 0
+    },
+    {
+        q:"Are you Mr Smith? – .......  .",
+        options:["No, I aren’t", "No, I’m not", "No, I don’t", "Yes, You are"],
+        answer: 1
+    },
+    {
+        q:"It ....... 10 o’clock.",
+        options:["be", "are", "is", "now"],
+        answer: 2   
+    },
+    {
+        q:"How often ....... you travel? – Every year.",
+        options:["are", "have", "does", "do"],
+        answer: 3
+    },
+    {
+        q:"Tom is ....... .",
+        options:["my sister’s friend", "friend my sister", "friend my sister’s", "my sister friend"],
+        answer: 0
+    },
+    {
+        q:"Mike ....... in our company.",
+        options:["is work", "works", "working", "work"],
+        answer: 1
+    },
+    {
+        q:"Is Ann ....... home?",
+        options:["on", "in", "at", "from"],
+        answer: 2
+    },
+    {
+        q:"This is ....... lake in the world.",
+        options:["beautifu", "more beautiful", "the beautifullest", "the most beautiful"],
+        answer: 3
+    },
+    {
+        q:"Mr Smith is ....... teacher.",
+        options:["a", "the", "-", "an"],
+        answer: 0
+    },
+    {
+        q:"........ many books on my desk.",
+        options:["Are", "There are", "There is", "These are"],
+        answer: 1
+    },
+    {
+        q:"We ....... drink alcohol.",
+        options:["haven't", "doesn't", "don’t", "not"],
+        answer: 2
+    },
+    {
+        q:"....... got a cat.",
+        options:["I'm", "I don't", "I like", "I've"],
+        answer: 3
+    },
+    {
+        q:"These are Jack and Mark.  ....... our friends.",
+        options:["They're", "He's", "We're", "There are"],
+        answer: 0
+    },
+    {
+        q:"....... are not my keys.",
+        options:["That", "These", "It", "They"],
+        answer: 1
+    },
+    {
+        q:"His living room is ....... than his bedroom.",
+        options:["more bigger", "the biggest", "bigger", "biggest"],
+        answer: 2
+    },
+    {
+        q:"She never ....... at 6 o’clock.",
+        options:["get up", "doesn’t get up", "does get up", "gets up"],
+        answer: 3
+    }
+
+]
